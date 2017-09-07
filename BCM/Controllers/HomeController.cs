0e74fc28520d41db6bf4f6e2022d75e95a6a89e0
Jay Creasy <j2thec32@gmail.com>
2017-09-07 10:53:06 +0100

    chan

--- BCM/Controllers/HomeController.cs
+++ BCM/Controllers/HomeController.cs
@@ -11,19 +11,43 @@ namespace BCM.Controllers
     public class HomeController : Controller
     {
         ContactManagerEntities db = new ContactManagerEntities();
-        public ActionResult Index(string option, string search, int? pageNumber)
+        public ActionResult Index(string option, string search, int? pageNumber, string sort)
         {
+            ViewBag.SortByName = string.IsNullOrEmpty(sort) ? "descending name" : "";
+            ViewBag.SortBycompanyName = sort == "CompanyName" ? "descending companyname" : "CompanyName";
+
+            var records = db.Contacts.AsQueryable();
+
             if(option == "CompanyName")
             {
-                return View(db.Contacts.Where(x => x.CompanyName.Contains(search) || search == null).ToList().ToPagedList(pageNumber ?? 1, 10));
+                records = records.Where(x => x.CompanyName.Contains(search) || search == null);
             } else if (option == "Surname")
             {
-                return View(db.Contacts.Where(x => x.Surname.StartsWith(search) || search == null).ToList().ToPagedList(pageNumber ?? 1, 10));
+                records = records.Where(x => x.Surname.StartsWith(search) || search == null);
             }
             else
             {
-                return View(db.Contacts.Where(x => x.FirstName.StartsWith(search) || search == null).ToList().ToPagedList(pageNumber ?? 1, 10));
+                records = records.Where(x => x.FirstName.StartsWith(search) || search == null);
             }
+            switch (sort)
+            {
+                case "descending name":
+                    records = records.OrderByDescending(x => x.FirstName);
+                    break;
+
+                case "descending companyname":
+                    records = records.OrderByDescending(x => x.CompanyName);
+                    break;
+
+                case "CompanyName":
+                    records = records.OrderByDescending(x => x.CompanyName);
+                    break;
+
+                default:
+                    records = records.OrderBy(x => x.FirstName);
+                    break;
+            }
+            return View(records.ToPagedList(pageNumber ?? 1, 10));
         }
     }
 }
