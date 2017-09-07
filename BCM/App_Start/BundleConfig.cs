55e383e9d8ed6c06ecf0818d0a5310f6e8b3fc75
Jay Creasy <j2thec32@gmail.com>
2017-09-07 09:26:21 +0100

    Initial Commit - with slight improvements

--- BCM/App_Start/BundleConfig.cs
+++ BCM/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace BCM
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cyborg.css",
                       "~/Content/site.css"));
         }
     }
--- BCM/Controllers/HomeController.cs
+++ BCM/Controllers/HomeController.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BCM.Models;
+using PagedList;
 
 namespace BCM.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        ContactManagerEntities db = new ContactManagerEntities();
+        public ActionResult Index(string option, string search, int? pageNumber)
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            if(option == "CompanyName")
+            {
+                return View(db.Contacts.Where(x => x.CompanyName.Contains(search) || search == null).ToList().ToPagedList(pageNumber ?? 1, 10));
+            } else if (option == "Surname")
+            {
+                return View(db.Contacts.Where(x => x.Surname.StartsWith(search) || search == null).ToList().ToPagedList(pageNumber ?? 1, 10));
+            }
+            else
+            {
+                return View(db.Contacts.Where(x => x.FirstName.StartsWith(search) || search == null).ToList().ToPagedList(pageNumber ?? 1, 10));
+            }
         }
     }
 }
