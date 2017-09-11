5587a9fb41dafa41559931aa18cf3ba0fc2cdbc4
Simon Foster <funkysi1701@gmail.com>
2017-09-11 19:06:30 +0100

    Add Contacts Database

---
+++ Contacts/dbo/Tables/Contacts.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[Contacts] (
+    [FirstName]   VARCHAR (256)  NOT NULL,
+    [Surname]     VARCHAR (256)  NOT NULL,
+    [CompanyName] VARCHAR (256)  NULL,
+    [Address]     NVARCHAR (256) NULL,
+    [Address2]    NVARCHAR (256) NULL,
+    [Address3]    NVARCHAR (256) NULL,
+    [City]        NVARCHAR (64)  NULL,
+    [County]      NVARCHAR (64)  NULL,
+    [Country]     NVARCHAR (64)  NULL,
+    [PostCode]    NVARCHAR (16)  NULL,
+    [Telephone]   NVARCHAR (32)  NULL,
+    [Telephone1]  NVARCHAR (32)  NULL,
+    [Telephone2]  NVARCHAR (32)  NULL,
+    [Telephone3]  NVARCHAR (32)  NULL,
+    [Telephone4]  NVARCHAR (32)  NULL,
+    [Email]       NVARCHAR (256) NULL,
+    [Email2]      NVARCHAR (256) NULL,
+    [Website]     NVARCHAR (256) NULL,
+    [Notes]       NVARCHAR (MAX) NULL,
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    CONSTRAINT [PK_Contacts] PRIMARY KEY CLUSTERED ([FirstName] ASC, [Surname] ASC, [Id] ASC)
+);
+
